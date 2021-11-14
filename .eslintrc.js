2f640001de85f5dc54238d30bc10912353e6f391
Brandon Let's Go <daniel@letsgobrandoncoin.org>
2021-11-14 20:36:05 +0000

    :arrow_up: Update dependecies and linting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-  ignorePatterns: ['coverage/'],
-  env: {
-    es2021: true,
-    mocha: true
-  },
-  extends: [
-    'standard'
-  ],
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  rules: {
+    env: {
+      es2021: true,
+      mocha: true
+    },
+    // extends: [
+    //   'standard'
+    // ],
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+      requireConfigFile: false
+    },
+    rules: {
+    }
   }
-}
+  
